47ff3cad8bbe1447b63e62f59acce365e60f094d
D3FaltXD <kalistoroy619@gmail.com>
2022-10-21 23:56:29 +0530

    Added Tools and Language

--- js/afterload.js
+++ js/afterload.js
@@ -2,8 +2,10 @@
 const Form1 = document.getElementById('Form1'),
   Form2 = document.getElementById('Form2'),
   Form3 = document.getElementById('Form3'),
+  Form4 = document.getElementById('Form4'),
   Next1 = document.getElementById('Next1'),
   Next2 = document.getElementById('Next2'),
+  Next3 = document.getElementById('Next3'),
   Back1 = document.getElementById('Back1'),
   Back2 = document.getElementById('Back2'),
   progress = document.getElementById('progress');
@@ -11,20 +13,30 @@ const Form1 = document.getElementById('Form1'),
 Next1.onclick = function () {
   Form1.style.display= 'none';
   Form2.style.display = 'block';
-  progress.style.width = '270px';
+  progress.style.width = '240px';
 };
 Back1.onclick = function () {
   Form1.style.display = 'block';
   Form2.style.display = 'none';
-  progress.style.width = '135px';
+  progress.style.width = '125px';
 };
 Next2.onclick = function () {
   Form2.style.display = 'none';
   Form3.style.display = 'block';
-  progress.style.width = '500px';
+  progress.style.width = '350px';
 };
 Back2.onclick = function () {
   Form2.style.display = 'block';
   Form3.style.display = 'none';
   progress.style.width = '250px';
+};
+Next3.onclick = function () {
+  Form3.style.display = 'none';
+  Form4.style.display = 'block';
+  progress.style.width = '500px';
+};
+Back3.onclick = function () {
+  Form3.style.display = 'block';
+  Form4.style.display = 'none';
+  progress.style.width = '350px';
 };
